e25285c32c56b9c0c4237be0d02f70a80fda8013
LY-1113 <88862844+LY-1113@users.noreply.github.com>
2026-02-27 01:46:56 -0800

    Add files via upload

--- scripts/DQN.py
+++ scripts/DQN.py
@@ -136,6 +136,34 @@ def batch_to_torch(batch):
 
     return boards, hands, acts, rews, nboards, nhands, dones, nmask
 
+#Prioritised relay
+def dqn_update_per(online_net, target_net, optimizer, batch, weights, gamma=0.99):
+    
+    boards, hands, acts, rews, nboards, nhands, dones, nmask = batch_to_torch(batch)
+
+    q_all = online_net(boards, hands)                         # (B,300)
+    q_sa  = q_all.gather(1, acts.unsqueeze(1)).squeeze(1)     # (B,)
+
+    with torch.no_grad():
+        q_next_all = target_net(nboards, nhands)              # (B,300)
+        q_next_all[~nmask] = -1e9
+        max_next = q_next_all.max(dim=1).values               # (B,)
+        y = rews + gamma * (1.0 - dones) * max_next           # (B,)
+
+    td = (y - q_sa)                                           # (B,)
+
+    # elementwise huber, then weight
+    per_item = F.smooth_l1_loss(q_sa, y, reduction="none")    # (B,)
+    loss = (weights * per_item).mean()
+
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(online_net.parameters(), 10.0)
+    optimizer.step()
+
+    #return float(loss.item()), td.detach().cpu().numpy() #uniform
+    return float(loss.item()), td.detach().abs().cpu() 
+
 def dqn_update(online_net, target_net, optimizer, batch, gamma=0.99):
     boards, hands, acts, rews, nboards, nhands, dones, nmask = batch_to_torch(batch)
 
@@ -162,6 +190,69 @@ def dqn_update(online_net, target_net, optimizer, batch, gamma=0.99):
 
     return float(loss.item())
 
+class PrioritizedReplayBuffer:
+    def __init__(self, capacity=100_000, alpha=0.6, eps=1e-6):
+        self.capacity = capacity
+        self.alpha = alpha
+        self.eps = eps
+
+        self.buf = [None] * capacity
+        self.priorities = np.zeros(capacity, dtype=np.float32)
+
+        self.pos = 0
+        self.size = 0
+
+    def add(self, obs, action, reward, next_obs, done, next_mask):
+        data = (
+            obs["board"].copy(),
+            obs["hand_masks"].copy(),
+            int(action),
+            float(reward),
+            next_obs["board"].copy(),
+            next_obs["hand_masks"].copy(),
+            bool(done),
+            next_mask.copy()
+        )
+
+        self.buf[self.pos] = data
+
+        # new samples get max priority so they will be learned soon
+        max_p = self.priorities[:self.size].max() if self.size > 0 else 1.0
+        self.priorities[self.pos] = max_p
+
+        self.pos = (self.pos + 1) % self.capacity
+        self.size = min(self.size + 1, self.capacity)
+
+    def sample(self, batch_size, beta=0.4):
+        assert self.size > 0
+
+        # compute sampling probability
+        p = self.priorities[:self.size] + self.eps
+        p = p ** self.alpha
+        #probs = p / p.sum() #uniform
+        probs = (p / p.sum()).astype(np.float32)
+
+        idxs = np.random.choice(self.size, batch_size, p=probs)
+        batch = [self.buf[i] for i in idxs]
+
+        # importance sampling weights
+        weights = (self.size * probs[idxs]) ** (-beta)
+        weights /= weights.max()
+
+        weights = torch.tensor(weights.tolist(), dtype=torch.float32, device=DEVICE)
+        #weights = torch.as_tensor(weights, dtype=torch.float32, device=DEVICE) #uniform
+
+        return batch, idxs, weights
+
+    def update_priorities(self, idxs, td_errors):
+        
+        td_errors = np.asarray(td_errors, dtype=np.float32)
+        new_p = np.abs(td_errors) + self.eps
+        self.priorities[idxs] = new_p
+
+    def __len__(self):
+        return self.size
+    
 def greedy_action(net, obs, mask):
     legal = np.flatnonzero(mask)
     if len(legal) == 0:
@@ -174,7 +265,7 @@ def greedy_action(net, obs, mask):
     
 if __name__ == "__main__":
 
-
+    USE_PER = True #PER USE
     best_return = -float("inf")
     best_step = 0
     #Creates the 1010 environment: board + bag + game + gym wrapper.
@@ -186,14 +277,21 @@ if __name__ == "__main__":
 
     optimizer = optim.Adam(online.parameters(), lr=1e-3)
     rb = ReplayBuffer(capacity=100_000)
+    if USE_PER:
+        rb = PrioritizedReplayBuffer(capacity=100_000, alpha=0.6)
+    else:
+        rb = ReplayBuffer(capacity=100_000)
 
     #NUM_EPISODES = 1000       # <- stop condition
     batch_size = 64
     gamma = 0.99
     ep = 0  #training purposes
 
-    MAX_STEPS = 300_000   # total transitions to collect (tune this)
+    MAX_STEPS = 300000   # total transitions to collect (tune this)
     #NUM_EPISODES = 0 
+    #start_learning = 200
+    #target_update_every = 500
+    #epsilon_decay_steps = 2000
 
     start_learning = 10000       # wait until buffer has enough
     train_every = 4             # learn every N environment steps
@@ -204,6 +302,7 @@ if __name__ == "__main__":
     epsilon_decay_steps = 300000
 
     global_step = 0
+
 
     while global_step < MAX_STEPS: #for training purpose
     #for ep in range(NUM_EPISODES):
@@ -247,8 +346,19 @@ if __name__ == "__main__":
 
             # learn
             if len(rb) >= start_learning and global_step % train_every == 0:
-                batch = rb.sample(batch_size) #Uniform Experience Replay
-                loss = dqn_update(online, target, optimizer, batch, gamma=gamma)
+                if not USE_PER:
+                    batch = rb.sample(batch_size)
+                    loss = dqn_update(online, target, optimizer, batch, gamma=gamma)
+
+                else:
+                    beta0, beta1 = 0.4, 1.0
+                    frac = min(1.0, global_step / MAX_STEPS)
+                    beta = beta0 + frac * (beta1 - beta0)
+
+                    batch, idxs, weights = rb.sample(batch_size, beta=beta)
+                    loss, td_errors = dqn_update_per(online, target, optimizer, batch, weights, gamma=gamma)
+                    #rb.update_priorities(idxs, td_errors) uniform
+                    rb.update_priorities(idxs, td_errors.tolist())
 
             # update target network
             if global_step % target_update_every == 0:
@@ -264,6 +374,7 @@ if __name__ == "__main__":
         
 
         # Watch one greedy game (prints ASCII each step)
+        
         WATCH_EVERY = 10       # watch every 10 episodes
         WATCH_MAX_STEPS = 300
         WATCH_DELAY = 0.05
@@ -284,7 +395,7 @@ if __name__ == "__main__":
                     break
 
                 obs_w, r, terminated, truncated, info_w = env_watch.step(a)
-                env_watch.render()
+                #env_watch.render()
                 time.sleep(WATCH_DELAY)
 
                 done_w = bool(terminated or truncated)
